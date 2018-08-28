24abd2604d800e026342cd085b1d4f9e1a111da8
Chris Kelley <chkelley@Chriss-MacBook-Pro-2.local>
2018-08-28 14:59:35 -0500

    style edits

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = (props) => {
   return (
-    <nav className="navbar navbar-default">f
+    <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
