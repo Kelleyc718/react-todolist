e69fc48b3623c2fd100f3560999a274de2569d0f
Chris Kelley <chkelley@Chriss-MacBook-Pro-2.local>
2018-08-28 14:24:53 -0500

    first commit

---
+++ automated.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo setenforce 0
+sudo yum update -y
+sudo yum install -y git docker-engine
+sudo systemctl start docker
+sudo usermod -aG docker opc
+sudo su opc
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
--- backend/app.js
+++ backend/app.js
@@ -9,21 +9,21 @@ var basePath = '/todo';
 var port = 6200;
 
 // Connection to DB
-// mongoose.connect('mongodb://mongodb')
-//     .then(() => {
-//       console.log('Backend Started');
-//     })
-//     .catch(err => {
-//         console.error('Backend error:', err.stack);
-//         process.exit(1);
-//     });
+
 
 // Routes and Backend Funcioncalities
 var todoListRoutes = require('./src/routes/todoListRoutes');
 
 // App Instance
 var app = express();
-app.use(express.static('public'));
+app.use(express.static('pmongoose.connect(\'mongodb://mongodb\')\n' +
+    '    .then(() => {\n' +
+    '      console.log(\'Backend Started\');\n' +
+    '    })\n' +
+    '    .catch(err => {\n' +
+    '        console.error(\'Backend error:\', err.stack);\n' +
+    '        process.exit(1);\n' +
+    '    });ublic'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
